9da627a3ca1cac6bc136a95686c111e4ee901e05
José Medrano <jose.medrano@xamarin.com>
2017-02-10 02:33:11 +0100

    some fixes in build script

--- make.sh
+++ make.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# nuget restoring
+if [ ! -f ./tools/nuget.exe ]; then
+	mkdir -p tools
+    echo "nuget.exe not found! downloading latest version"
+    curl -O https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+    mv nuget.exe tools/
+fi
+
 echo "Restoring xamarinstudio.raspberry nugets..."
-mono tools/nuget.exe restore src/XamarinStudio.Raspberry.sln
+mono tools/nuget.exe restore src/MonoDevelop.Debug.CocoaLayout.sln
 
 echo "build project in release mode"
 xbuild /p:Configuration=Release src/MonoDevelop.Debug.CocoaLayout.sln 
